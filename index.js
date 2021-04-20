f8c300b07338d902b17f1e3b9ed4186f023ce39f
greencodedev <greencodedev912@gmail.com>
2021-04-20 15:42:58 +0800

    fix cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors')
 
 const app = express();
 
 // configure our express instance with some body-parser settings
 // including handling JSON data
 app.use(bodyParser.json());
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const routes = require('./router/routes.js')(app);
